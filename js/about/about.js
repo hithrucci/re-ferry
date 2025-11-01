13f2173e89b1548fe40ada4a791798faf1b69fb3
BuildTools <unconfigured@null.spigotmc.org>
2025-11-01 15:32:21 +0900

    251101 수정2

--- js/about/about.js
+++ js/about/about.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("../../header.html")
+  const basePath =
+    location.hostname === "hithrucci.github.io" ? "/re-ferry" : "";
+  fetch(`${basePath}/header.html`)
     .then((res) => res.text())
     .then((data) => (document.querySelector("header").innerHTML = data));
+
+  fetch(`${basePath}/footer.html`)
+    .then((res) => res.text())
+    .then((data) => (document.querySelector("footer").innerHTML = data));
 });
 
 gsap.registerPlugin(ScrollTrigger);
